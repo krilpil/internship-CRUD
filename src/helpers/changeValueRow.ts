49dc44275623279e020f0195df0c0c824b58c8bf
Kirill Bespalov <kesha_bespalov@mail.ru>
2022-04-11 20:24:17 +0300

    Refactoring helper - changeValueRow

--- src/helpers/changeValueRow.ts
+++ src/helpers/changeValueRow.ts
@@ -3,27 +3,12 @@ import {TableRowProps} from "../types/table";
 
 const changeValueRow = (event: React.ChangeEvent<HTMLInputElement>, row: TableRowProps)
     : TableRowProps => {
-    switch (event.currentTarget.name) {
-        case 'name':
-            return ({
-                name: event.currentTarget.value,
-                age: row.age,
-                email: row.email
-            })
-        case 'age':
-            return ({
-                name: row.name,
-                age: event.currentTarget.value,
-                email: row.email
-            })
-        case 'email':
-            return ({
-                name: row.name,
-                age: row.age,
-                email: event.currentTarget.value
-            })
-        default:
-            return row
+
+    const {name, value} = event.currentTarget
+
+    return {
+        ...row,
+        [name]: value
     }
 }
 
